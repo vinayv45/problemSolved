16cdba0b03082a5d4a1b7c5bccc8474a38f614b6
vinayv45 <vinayvishwakarma920@gmail.com>
2024-10-16 02:01:54 +0530

    push code

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
     subStage: '',
     stage: '',
     stageLevel: 0,
+    subStageLevel: 0,
   );
 
   @override
@@ -45,20 +46,18 @@ class _HomeScreenState extends State<HomeScreen> {
     jsonString = data2;
     final data = jsonDecode(jsonString);
 
-    final currentStage = data['data']['applicationDetails'][0]['stage'];
-    final currentSubStage = data['data']['applicationDetails'][0]['subStage'];
+    final currentStage = "Qualification";
+    final currentSubStage = "Asset Details";
 
-    // Debug output
-    print("Current Stage: $currentStage");
-    print("Current SubStage: $currentSubStage");
-
-    // Extracting stage level
+    // Extracting stage level and sub-stage level
     int currentStageLevel = 0;
+    int currentSubStageLevel = 0;
     for (var step in data["data"]["applicationContent"][0]["subStages"]) {
       if (step["SysStage"] == currentStage &&
           step["SystemSubStage"] == currentSubStage) {
         currentStageLevel = int.parse(step["stageLevel"].toString());
-        break; // Exit once we find the matching step
+        currentSubStageLevel = int.parse(step["subStageLevel"].toString());
+        break;
       }
     }
 
@@ -67,31 +66,25 @@ class _HomeScreenState extends State<HomeScreen> {
         subStage: currentSubStage,
         stage: currentStage,
         stageLevel: currentStageLevel,
+        subStageLevel: currentSubStageLevel,
       );
     });
 
-    // Debug output for currentStatus
-    print(
-        "Current Status: ${currentStatus.subStage}, ${currentStatus.stage}, ${currentStatus.stageLevel}");
-
-    // Group step data
     groupedSteps =
         groupByPlanetStage(data["data"]["applicationContent"][0]["subStages"]);
     setState(() {});
-
-    // Debug output for grouped steps
-    print("Grouped Steps: $groupedSteps");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TimelineStepper(
-            groupedSteps: groupedSteps,
-            currentStatus: currentStatus,
-          )),
+        padding: const EdgeInsets.all(16.0),
+        child: TimelineStepper(
+          groupedSteps: groupedSteps,
+          currentStatus: currentStatus,
+        ),
+      ),
     );
   }
 
@@ -114,11 +107,13 @@ class StepData {
   final String subStage;
   final String stage;
   final int stageLevel;
+  final int subStageLevel;
 
   StepData({
     required this.subStage,
     required this.stage,
     required this.stageLevel,
+    required this.subStageLevel,
   });
 }
 
@@ -168,6 +163,7 @@ class TimelineStepper extends StatelessWidget {
       itemBuilder: (context, index) {
         final String planetStage = groupedSteps.keys.elementAt(index);
         final List<StepperItem> subSteps = groupedSteps[planetStage]!;
+        bool isLastElement = index == groupedSteps.length - 1;
 
         // Determine if the planet stage is complete
         bool isStageComplete = subSteps
@@ -175,171 +171,137 @@ class TimelineStepper extends StatelessWidget {
 
         // Only show ongoing sub-stages for the current stage
         final List<StepperItem> ongoingSubStages = subSteps.where((step) {
-          return step.stageLevel ==
-              currentStatus.stageLevel; // Only include ongoing stages
+          return step.stageLevel == currentStatus.stageLevel;
         }).toList();
 
-        bool arePreviousStagesComplete =
-            groupedSteps.keys.take(index).every((key) {
-          final previousSteps = groupedSteps[key]!;
-          return previousSteps.every((step) {
-            return step.stageLevel < currentStatus.stageLevel;
-          });
-        });
-
-        print(
-            "Are Previous Stages Complete for index $index: $arePreviousStagesComplete");
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Planet stage label
             Padding(
-              padding: const EdgeInsets.only(
-                left: 20.0,
-                bottom: 0,
-              ),
-              child: Row(
+              padding: const EdgeInsets.only(left: 20.0, bottom: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    margin: EdgeInsets.zero,
-                    width: 16,
-                    height: 16,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: isStageComplete
-                          ? Colors.blue
-                          : Colors.transparent, // Filled if complete
-                      border: Border.all(
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.zero,
+                        width: 16,
+                        height: 16,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
                           color: isStageComplete
-                              ? Colors.transparent
-                              : Colors.grey), // Outline if not complete
-                    ),
-                    child: isStageComplete
-                        ? const Icon(
-                            Icons.check,
-                            color: Colors.white, // Check icon color
-                            size: 12, // Adjust size as needed
-                          )
-                        : null, // No icon for incomplete stages
-                  ),
-                  const SizedBox(width: 10),
-                  Text(
-                    planetStage,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
+                              ? Colors.blue
+                              : Colors.transparent,
+                          border: Border.all(
+                            color: isStageComplete
+                                ? Colors.transparent
+                                : Colors.grey,
+                          ),
+                        ),
+                        child: isStageComplete
+                            ? const Icon(
+                                Icons.check,
+                                color: Colors.white,
+                                size: 12,
+                              )
+                            : null,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        planetStage,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
 
-            if (isStageComplete) ...[
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 26.0,
-                  bottom: 0,
-                  top: 0,
-                ),
-                child: Column(
-                  children: List.generate(
-                    4,
-                    (index) {
-                      return Container(
-                        height: 10,
-                        width: 2,
-                        color: Colors.blue,
-                        margin: const EdgeInsets.only(
-                            right: 4), // Space between bars
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-
+            // Render ongoing sub-stages
             ...ongoingSubStages.map(
               (step) {
-                print("step ${step.systemSubStage}");
                 bool isActive = step.systemSubStage == currentStatus.subStage &&
                     step.sysStage == currentStatus.stage;
+                bool isPrevious =
+                    step.subStageLevel <= currentStatus.subStageLevel;
 
-                return step.planetSub.isEmpty
-                    ? const SizedBox.shrink()
-                    : Padding(
+                return step.planetSub.isNotEmpty
+                    ? Padding(
                         padding: const EdgeInsets.only(
                           left: 24,
                           top: 0,
                         ),
                         child: Row(
                           children: [
-                            // Check if previous stages are complete to show grey or blue dots
-                            arePreviousStagesComplete
-                                ? Stack(
-                                    alignment: Alignment.center,
-                                    children: [
-                                      Column(
-                                        children: List.generate(
-                                          4,
-                                          (index) {
-                                            return Container(
-                                              width: 2,
-                                              height: 10, // Height of each dot
-                                              color: Colors.blue,
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 8,
-                                        height: 8,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                : Stack(
-                                    alignment: Alignment.center,
-                                    children: [
-                                      Column(
-                                        children: List.generate(
-                                          4,
-                                          (index) {
-                                            return Container(
-                                              width: 2,
-                                              height: 10, // Height of each dot
-                                              color: Colors.grey,
-                                              margin: const EdgeInsets.only(
-                                                bottom: 4,
-                                              ), // Space between dots
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 8,
-                                        height: 8,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ],
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Column(
+                                  children: List.generate(
+                                    4, // Adjust the number of segments in the line
+                                    (index) {
+                                      return Container(
+                                        width: 2,
+                                        height:
+                                            10, // Height of each line segment
+                                        color: isPrevious
+                                            ? Colors.blue
+                                            : Colors
+                                                .grey, // Blue or grey based on completion
+                                        margin: EdgeInsets.only(
+                                          bottom: isPrevious ? 0 : 4,
+                                        ), // Space between segments
+                                      );
+                                    },
                                   ),
+                                ),
+                                Container(
+                                  width: 8,
+                                  height: 8,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: isPrevious
+                                        ? Colors.blue
+                                        : Colors.grey, // Blue or grey dot
+                                  ),
+                                ),
+                              ],
+                            ),
                             const SizedBox(width: 8),
                             Text(
-                              step.systemSubStage,
+                              step.planetSub,
                               style: TextStyle(
-                                color: isActive ? Colors.black : Colors.grey,
-                                fontSize: 16,
+                                color: isActive
+                                    ? Colors.black
+                                    : Colors
+                                        .grey, // Black for active, grey for inactive
+                                fontSize: 12,
                               ),
                             ),
                           ],
                         ),
-                      );
+                      )
+                    : const SizedBox.shrink();
               },
+            ),
+
+            // Red line to separate each planet stage
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 26.0,
+              ),
+              child: isLastElement
+                  ? Container() // No red line for the last element
+                  : Container(
+                      width: 2,
+                      height: 40, // Adjust height based on your design
+                      color: Colors.red,
+                    ),
             ),
           ],
         );
